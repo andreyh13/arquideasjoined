71dc8764dcb5c4f050523b387ffd892b1b17d684
hdeleon <hdeleon@nivaria.com>
2013-02-21 10:18:25 +0000

    Update templates/node/node-inscription.tpl.php
    ARQUIDEAS-1205 # Add information text for payment pending inscriptions

--- templates/node/node-inscription.tpl.php
+++ templates/node/node-inscription.tpl.php
@@ -192,10 +192,16 @@
         </div>
     </div>
 
-    
+
     <?php endif; ?>
     <!-- End Introduction text -->
 
+    <!-- Introduction text for payment pending inscriptions -->
+    <?php if($node->field_inscription_state[0]['value']==InscriptionState::INSCRIPTION_PAYMENT_PENDING) ?>
+    <div class="introduction">
+        <?php print t('Once we receive your payment by transfer we will send you an email confirming your registration in the competition. Thank you!'); ?>
+    </div>
+    <!-- End Introduction text for payment pending inscriptions -->
 
     <!-- INSCRIPTION, PAYMENT OR PRESENTATION LINK -->
     <?php
