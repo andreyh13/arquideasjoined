360c36b849e9448d8c0a5f7a73042e52301c38b0
Andriy Khomenko <andriy@nivaria.com>
2013-04-22 17:45:37 +0100

    ARQUIDEAS-1522 # Social networks widget. Mail button & styling

--- scripts/arquideas.js
+++ scripts/arquideas.js
@@ -439,5 +439,7 @@ $(window).load(function(){
       }
       $("#block-arquideas_generic-13 .widget-body .addthis_toolbox .addthis_toolbox_item > iframe").css("width",w);
       $("#block-arquideas_generic-13 .widget-body .addthis_toolbox > .fb_send").css("width",w);
+      $("#block-arquideas_generic-13 .widget-body .addthis_toolbox .addthis_toolbox_item > .fb-like").css("width",w);
+      $("#block-arquideas_generic-13 .widget-body .addthis_toolbox .addthis_toolbox_item > iframe").css("height","20px");
     }    
 });
