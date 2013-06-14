b7636be94b71eed5013692b555e8da1c4cc8e8c7
Andriy Khomenko <andriy@nivaria.com>
2013-06-14 12:05:53 +0100

    ARQUIDEAS-1639 # Team members for free competitions in inscription page

--- templates/node/node-inscription.tpl.php
+++ templates/node/node-inscription.tpl.php
@@ -17,8 +17,11 @@
                     $is_individual = FALSE;
                 }
             }
-        }    
-
+        }  
+        if($contest->field_contest_free[0]['value']==1){
+            $is_individual = FALSE;
+        }
+        
     $class_add = '';
     if($node->field_inscription_state[0]['value']==InscriptionState::PREINSCRIPTED && $num_members==1){
         $class_add = 'launch-overlay';
